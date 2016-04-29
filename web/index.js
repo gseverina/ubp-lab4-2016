421d6e045ef6c81afbf66386afb39fe2d5481f72
Gaston Severina <gaston.e.severina@intel.com>
2016-04-29 17:31:03 -0300

    creating first version of the web app and the docker-compose

---
+++ web/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + "/" + "index.html")
+})
+
+var server = app.listen(80, '127.0.0.1', function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log("example app listening at http://%s:%s", host, port)
+})
+
