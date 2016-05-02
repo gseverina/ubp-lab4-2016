426cb63b33fa4eaadbbde802fc2d98bd1512e20d
Gaston Severina <gaston.e.severina@intel.com>
2016-05-01 21:50:50 -0300

    adding login and register support to the web app

--- web/index.js
+++ web/index.js
@@ -1,13 +1,45 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + "/" + "index.html")
-})
+app.use(bodyParser.urlencoded({ extended: true }));
 
-var server = app.listen(80, '127.0.0.1', function () {
-  var host = server.address().address
-  var port = server.address().port
-  console.log("example app listening at http://%s:%s", host, port)
-})
+app.get('/', function (req, res) {
+    res.redirect('/login');
+});
 
+app.get('/login', function(req, res) {
+    res.sendFile(__dirname + "/" + "index.html");
+});
+
+app.post('/login', function(req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    if(username == 'u' && password == 'p') {
+        res.send("OK");
+    }
+    else{
+        res.send("ERROR");
+    }
+});
+
+app.get('/register', function(req, res) {
+    res.sendFile(__dirname + "/" + "register.html");
+});
+
+app.post('/register', function(req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    if(username == 'u' && password == 'p') {
+        res.send("YA EXISTE");
+    }
+    else{
+        res.redirect('/login');
+    }
+});
+
+var server = app.listen(8080, '127.0.0.1', function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("example app listening at http://%s:%s", host, port);
+});
