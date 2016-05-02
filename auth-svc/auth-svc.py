c4b6ade151046a024279ccded4a72a2b1f7f72b8
Gaston Severina <gaston.e.severina@intel.com>
2016-05-02 17:06:03 -0300

    adding auth service using bottle as example

---
+++ auth-svc/auth-svc.py
@@ -0,0 +1,18 @@
+from bottle import Bottle, run, template
+
+app = Bottle()
+
+
+@app.route('/hello')
+def hello():
+    return "Hello World!"
+
+
+@app.route('/')
+@app.route('/hello/<name>')
+def greet(name='Stranger'):
+    return template('Hello {{name}}, how are you?', name=name)
+
+
+run(app, host='localhost', port=8080)
+
