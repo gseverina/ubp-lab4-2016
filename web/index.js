54b54d5e79ffc6b04a790ac3484a5c21bf76b901
Gaston Severina <gaston.e.severina@intel.com>
2016-05-06 13:16:25 -0300

    web verion 1 completed

--- web/index.js
+++ web/index.js
@@ -4,12 +4,24 @@ var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+/** DASHBOARD **/
+
 app.get('/', function (req, res) {
     res.redirect('/login');
 });
 
+
+/** LOGIN **/
+
 app.get('/login', function(req, res) {
-    res.sendFile(__dirname + "/" + "index.html");
+    res.sendFile(__dirname + "/" + "login.html");
+});
+
+app.get('/login/:id', function(req, res) {
+    var id = req.params.id;
+    console.log("id: %s", id);
+    res.sendFile(__dirname + "/" + "login-error.html");
 });
 
 app.post('/login', function(req, res) {
@@ -19,24 +31,36 @@ app.post('/login', function(req, res) {
         res.send("OK");
     }
     else{
-        res.send("ERROR");
+      res.redirect("/login/1");
     }
 });
 
+
+/** REGISTER **/
+
 app.get('/register', function(req, res) {
     res.sendFile(__dirname + "/" + "register.html");
+});
+
+app.get('/register/:id', function(req, res) {
+    var id = req.params.id;
+    console.log("id: %s", id);
+    res.sendFile(__dirname + "/" + "register-error.html");
 });
 
 app.post('/register', function(req, res) {
     var username = req.body.username;
     var password = req.body.password;
     if(username == 'u' && password == 'p') {
-        res.send("YA EXISTE");
+      res.redirect("/register/1");
     }
     else{
-        res.redirect('/login');
+        res.send("OK");
     }
 });
+
+
+/** SERVER **/
 
 var server = app.listen(8080, '127.0.0.1', function () {
   var host = server.address().address;
